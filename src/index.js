b6d7eb32596fdc0af2333140b29a9a75d5004141
Fernando <fernando.pavanello2@gmail.com>
2019-02-19 09:16:08 +0000

    [021] - Desafio Único

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const $root = document.querySelector("#root");
 const $memoryCard = document.createElement("article");
+const $memoryCardActive = document.createElement("article");
 const $icon = `
   <img
     src="img/icon-collabcode.png"
@@ -7,8 +8,23 @@ const $icon = `
     class="icon"
   />
 `;
+const $iconJs = `
+  <img
+    src="img/icon-js.png"
+    alt="JS Selected"
+    class="icon -active"
+  />
+`;
 
 $memoryCard.classList.add("memory-card");
 
+$memoryCardActive.classList.add("memory-card");
+$memoryCardActive.classList.add("-active");
+
 $root.insertBefore($memoryCard, null);
+$root.insertBefore($memoryCardActive, null);
+
 $memoryCard.insertAdjacentHTML("afterbegin", $icon);
+
+$memoryCardActive.insertAdjacentHTML("afterbegin", "<div class='ball'></div>");
+$memoryCardActive.insertAdjacentHTML("afterbegin", $iconJs);
