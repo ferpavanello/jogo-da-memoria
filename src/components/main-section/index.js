696fdff267b2d1a62c1bcd8474f8d4f1a2cbe86d
Fernando <fernando.pavanello2@gmail.com>
2019-03-19 09:16:32 +0000

    [039] - Desafio Intermediário + Avançado

--- src/components/main-section/index.js
+++ src/components/main-section/index.js
@@ -6,20 +6,51 @@ const mainSection = (function() {
     const $style = document.createElement("style");
 
     $style.textContent = `
+      .main-section {
+        position: relative;
+      }
+      
+      .main-div {
+        position: absolute;
+        display: flex;
+        flex-direction: column;
+        left: 50%;
+        margin-top: -30px;
+      }
+
       .main-icon {
         width: 170px;
-        height: 175px
+        height: 175px;
+        padding-bottom: 30px;
+        transform: translateX(-50%);
+        background-color: #fff;
+        box-shadow: 0 0 0 25px rgb(255, 255, 255);
+        border-radius: 48%;
+      }
+
+      .main-title {
+        color: #f25a70;
+        font-size: 24px;
+        font-weight: bold;
+        text-align: center;
+        text-transform: uppercase;
+        transform: translateX(-50%);
       }
     `;
 
     $head.insertAdjacentElement("beforeend", $style);
   };
 
-  module.render = () => {
+  module.render = content => {
     module._style();
 
     return `
-      <img src="img/icon-collabcode.png" class="main-icon"></img>
+      <section class="main-section">
+        <div class="main-div">
+          <img src="img/icon-collabcode.png" class="main-icon"></img>
+          <span class="main-title">Welcome!</span>
+        </div>
+      </section>
     `;
   };
 
